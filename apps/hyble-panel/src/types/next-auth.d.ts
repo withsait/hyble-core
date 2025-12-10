2398099747727807ec7e579f939084e082ff51cc
ahmetyvksait <ahmetyvksait00@gmail.com>
2025-12-10 10:09:51 +0300

    Add NextAuth type declarations for role property

---
+++ apps/hyble-panel/src/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+import { JWT, DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession["user"];
+    sessionToken?: string;
+  }
+
+  interface User extends DefaultUser {
+    role: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id: string;
+    role: string;
+    sessionToken?: string;
+  }
+}
